ef6a8fb407b137f404f0c3b5f8116c637290fbcf
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-25 20:36:59 +0000

    Release v1.0.1

--- core/request_option.go
+++ core/request_option.go
@@ -57,8 +57,8 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/Ryan-Amirthan/MovieAPI-GoSDK")
-	headers.Set("X-Fern-SDK-Version", "v1.0.0")
-	headers.Set("User-Agent", "github.com/Ryan-Amirthan/MovieAPI-GoSDK/1.0.0")
+	headers.Set("X-Fern-SDK-Version", "v1.0.1")
+	headers.Set("User-Agent", "github.com/Ryan-Amirthan/MovieAPI-GoSDK/1.0.1")
 	return headers
 }
 
